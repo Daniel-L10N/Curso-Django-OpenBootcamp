ded27230a014ce1c82be333b40f7b0f536db97ae
Daniel-L10N <alejandrolazolemus@gmail.com>
2025-05-03 21:22:57 -0600

    iniciando las plantillas

--- clase_1/clase_1/views.py
+++ clase_1/clase_1/views.py
@@ -6,6 +6,8 @@ def despedida(request):
     return HttpResponse('Hasta luego')
 
 def adulto(request, edad):
+    print(type(edad))
+    print(edad)
     if edad >=18:
         return HttpResponse('eres mayor de edad')
     else:
