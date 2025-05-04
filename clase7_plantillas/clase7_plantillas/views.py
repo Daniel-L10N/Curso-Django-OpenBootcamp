751f4ee79d99194e6f02739a20c1767b3f2e1eb6
Daniel-L10N <alejandrolazolemus@gmail.com>
2025-05-03 22:05:14 -0600

    PASAR DATOS A LAS PLANTILLAS DESDE LAS URL

--- clase7_plantillas/clase7_plantillas/views.py
+++ clase7_plantillas/clase7_plantillas/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def simple(request):
     return render(request, 'simple.html', {'nombre':'Daniel'})
+
+def dinamico(request, name):
+    print(type(name))
+    categorias = ['basos', 'copas', 'tamques']
+    print(name)
+    return render(request, 'dinamico.html', {'name':name, 'category': categorias})
